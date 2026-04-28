7c30ff96752ce2bec5a92a9352b457bfe0f9ce62
cesarecucchiarini <cucchiarini.cesare@franchettisalviani.net>
2026-04-28 17:43:04 +0200

    modifiche al manager atleti
    Ho modificato il modo in cui gli atleti cominciano a correre

--- src/threadrelay/GestoreAtleti.java
+++ src/threadrelay/GestoreAtleti.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 public class GestoreAtleti implements Observer{
     private ArrayList<Atleta> atleti = new ArrayList<>();
     private GestoreGrafica gestoreGrafica;
+    int counter;
     private boolean garaIniziata = false;
     
     public GestoreAtleti(int numeroAtleti){       
@@ -32,8 +33,9 @@ public class GestoreAtleti implements Observer{
         for(Atleta a : atleti){
             a.setDistanzaPercorsa(0);
         }
+        counter = 0;
         garaIniziata = true;
-        new Thread(atleti.get(0)).start();
+        new Thread(atleti.get(counter++)).start();
     }
 
     public synchronized void impostaVelocita(int velocita){
@@ -44,9 +46,10 @@ public class GestoreAtleti implements Observer{
     
     @Override
     public synchronized void update(Atleta atleta){
-        if(atleta.getDistanzaPercorsa() == 90 && atleti.indexOf(atleta) != atleti.size()-1)
-            new Thread(atleti.get(atleti.indexOf(atleta) + 1)).start();
-        if(atleta.getDistanzaPercorsa() == 100 && atleti.indexOf(atleta) == atleti.size()-1)
+        if(atleta.getDistanzaPercorsa() == 90 && counter != atleti.size())
+            new Thread(atleti.get(counter++)).start();
+        
+        else if(atleta.getDistanzaPercorsa() == 100 && counter == atleti.size())
             garaIniziata = false;
     }
 }
